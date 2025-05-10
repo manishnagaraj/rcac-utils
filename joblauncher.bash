7a19e59b05619020a9be29c2b2225dc9324770c4
amoghj98 <joshi157@purdue.edu>
2025-05-10 16:33:55 -0400

    add support for interactive jobs and cpu-only jobs, modify default job partition to cocosys

--- joblauncher.bash
+++ joblauncher.bash
@@ -35,13 +35,16 @@ FLAG=false
 MAIL=""
 MAIL_TYPE=BEGIN,END,FAIL,TIME_LIMIT_90
 
+# interactive job flag. DO NOT MODIFY
+INTERACTIVE=""
+
 # file name setup
 JOB_NAME="${USER}_%j"
 LOG_PATH="${HOME}/joboutput/"
 
 # usage help message
 usage() {
-	echo -e "\nusage: $0 [-h] [-j JOB_SUBMISSION_SCRIPT] [-t SCRIPT_TYPE] [-d SCRIPT_DIR] [-f SCRIPT_FILE] [-l LOG_PATH] [-e ENV_NAME] [-n JOB_NAME] [-g N_GPUS] [-c N_CPUS] [-q QUEUE] [-Q QoS] [-p PARTITION] [-T MAX_TIME] [-s SIG_INTERVAL] [-m]" 1>&2;
+	echo -e "\nusage: $0 [-h] [-j JOB_SUBMISSION_SCRIPT] [-t SCRIPT_TYPE] [-d SCRIPT_DIR] [-f SCRIPT_FILE] [-l LOG_PATH] [-e ENV_NAME] [-n JOB_NAME] [-g N_GPUS] [-c N_CPUS] [-q QUEUE] [-Q QoS] [-p PARTITION] [-T MAX_TIME] [-s SIG_INTERVAL] [-m] [-i]" 1>&2;
 	echo "-h: Display help message"
 	echo "-j JOB_SUBMISSION_SCRIPT: Name of job submission script. Defaults to 'jobsubmissionscript.sub'"
 	echo "-t SCRIPT_TYPE: Type of script to execute. Supported values: bash, python. Defaults to 'python'"
@@ -51,20 +54,21 @@ usage() {
 	echo "-e ENV_NAME: Name of the script's conda environment. Defaults to 'base'"
 	echo "-n JOB_NAME: Name of the job. Defaults to ${USER}_%j, where %j is the job number"
 	echo "-g N_GPUS: Number of GPU cards required. Defaults to 1"
-	echo -e "-c N_CPUS: Number of CPUs required. Defaults to 14.\n[${yellow}WARNING${nc}] Gautschi restricts N_CPUS to 14 per requested GPU. Supply this arg accordingly"
+	echo -e "-c N_CPUS: Number of CPUs required. Defaults to 14.\n\t[${yellow}WARNING${nc}] Gautschi restricts N_CPUS to 14 per requested GPU. Supply this arg accordingly"
 	echo "-q QUEUE: SLURM queue to launch job on. Supported values: kaushik, cocosys. Defaults to 'cocosys'"
 	echo "-Q QoS: Quality-of-Service to be associated with the job. Supported values: normal, preemptible. Defaults to 'normal'"
-	echo "-p PARTITION: Name of partition to run on. Defaults to 'ai'"
+	echo "-p PARTITION: Name of partition to run on. Defaults to 'cocosys'"
 	echo -e "-T MAX_TIME: Max job time. After executing for this much time, the job is killed.\n\tSpecify in dd-hh:mm:ss format. Defaults to 6:00:00 (6 hrs)"
-	echo -e "-s SIG_INTERVAL: SIGUSR1 is sent to the user script these many seconds before MAX_TIME is reached. Supported values: [0, 65535]. Defaults to 60.\n[${yellow}WARNING${nc}] Handling of OS signal is left to the user\n"
+	echo -e "-s SIG_INTERVAL: SIGUSR1 is sent to the user script these many seconds before MAX_TIME is reached. Supported values: [0, 65535]. Defaults to 60.\n\t[${yellow}WARNING${nc}] Handling of OS signal is left to the user\n"
 	echo "-m: Email notification flag. If set, sends email notification on job start, end, fail, and upon reaching 90% of specified job time limit"
+	echo "-i: Interactive Job Flag. If set, the requested resource set is allocated in interactive fashion. Allows for the spawning of multiple jobs on the same hardware resources\n\t[${yellow}WARNING${nc}] Setting this flag causes all arguments except the resource requesting arguments to be ignored."
 	exit 1;
 }
 
 # arg init
 N_GPUS=1
 N_CPUS=14
-PARTITION=ai
+PARTITION=cocosys
 QOS_LEVEL=normal
 MAX_TIME=6:00:00
 ENV_NAME=base
@@ -75,7 +79,7 @@ SCRIPT_FILE=helloWorld.py
 SIG_INTERVAL=60
 
 # read args
-while getopts "hj:t:d:f:l:e:n:g:c:q:Q:p:T:s:m" opts; do
+while getopts "hj:t:d:f:l:e:n:g:c:q:Q:p:T:s:mi" opts; do
 	case "${opts}" in
 		h)	usage;;
 		j)	JOB_SUBMISSION_SCRIPT=$OPTARG;;
@@ -93,6 +97,7 @@ while getopts "hj:t:d:f:l:e:n:g:c:q:Q:p:T:s:m" opts; do
 		T)	MAX_TIME=$OPTARG;;
 		s)	SIG_INTERVAL=$OPTARG;;
 		m)	MAIL=true;;
+		i)	INTERACTIVE=true;;
 		*)	usage;;
 	esac
 done
@@ -139,9 +144,12 @@ N_NODES=$(((($N_CPUS+$DIV-1))/$DIV))
 if [[ $N_GPUS -gt 0 ]]; then
 	REQ_CPUS_PER_GPU=$(($N_CPUS/$N_GPUS))
 else
-	# does not support cpu-only partitions right now, TODO
-	echo -e "[${red}FATAL${nc}] Launching CPU-only jobs not supported"
-	exit -1
+	CPU_ONLY_PARTITIONS=("highmem")
+	if [[ ! " ${CPU_ONLY_PARTITIONS[@]} " =~ " $PARTITION " ]]; then
+		# does not support cpu-only jobs on partitions other than highmem right now, TODO
+		echo -e "[${red}FATAL${nc}] Launching CPU-only jobs not supported on partition $PARTITION. CPU-only jobs can only be launched on partition(s): $CPU_ONLY_PARTITIONS"
+		exit -1
+	fi
 fi
 MAX_CPUS_PER_GPU=$(($DIV/$((${CLUSTER}"_gpu_"${PARTITION}))))
 if [[ $REQ_CPUS_PER_GPU -gt $MAX_CPUS_PER_GPU ]] && [[ $PARTITION == "cocosys" ]]; then
@@ -180,9 +188,17 @@ MAIL_ARGS="--mail-type=${MAIL_TYPE} --mail-user=${USER}@purdue.edu"
 #	-A [QUEUE] : Name of queue to submit job to. H200s are accessed using queue "cocosys". NRL's private queue is named "kaushik"
 #
 #	For more info about sbatch, consult the sbatch man page using "man sbatch"
-sbatch \
-	-p $PARTITION -q $QOS_LEVEL \
-	${MAIL:+"$MAIL_ARGS"} \
-	--job-name=$JOB_NAME --output="${OUT_FILE}.log" --error="${ERR_FILE}.log" \
-	--gpus-per-node=$N_GPUS --gres=gpu:$N_GPUS -t $MAX_TIME --signal=B:SIGUSR1@${SIG_INTERVAL} --nodes=$N_NODES --cpus-per-gpu=$CPUS_PER_GPU -A $QUEUE \
-	$JOB_FILE_PATH/${JOB_SUBMISSION_SCRIPT} -e $ENV_NAME -t $SCRIPT_TYPE -d $SCRIPT_DIR -f $SCRIPT_FILE
+if [[ ! $INTERACTIVE ]]; then
+	sbatch \
+		-p $PARTITION -q $QOS_LEVEL \
+		${MAIL:+"$MAIL_ARGS"} \
+		--job-name=$JOB_NAME --output="${OUT_FILE}.log" --error="${ERR_FILE}.log" \
+		--gpus-per-node=$N_GPUS --gres=gpu:$N_GPUS -t $MAX_TIME --signal=B:SIGUSR1@${SIG_INTERVAL} --nodes=$N_NODES --cpus-per-gpu=$CPUS_PER_GPU -A $QUEUE \
+		$JOB_FILE_PATH/${JOB_SUBMISSION_SCRIPT} -e $ENV_NAME -t $SCRIPT_TYPE -d $SCRIPT_DIR -f $SCRIPT_FILE
+else
+	salloc \
+		-p $PARTITION -q $QOS_LEVEL \
+		${MAIL:+"$MAIL_ARGS"} \
+		--job-name=$JOB_NAME \
+		--gpus-per-node=$N_GPUS --gres=gpu:$N_GPUS -t $MAX_TIME --signal=R:SIGUSR1@${SIG_INTERVAL} --nodes=$N_NODES --cpus-per-gpu=$CPUS_PER_GPU -A $QUEUE
+fi
